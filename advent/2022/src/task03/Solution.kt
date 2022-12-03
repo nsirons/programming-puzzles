cd23091d85dade404e943494af6bcc41244d7bb4
Nikita Sirons <nsirons@amazon.com>
2022-12-03 11:20:03 +0100

    add aoc2022 day3, shorter

--- advent/2022/src/task03/Solution.kt
+++ advent/2022/src/task03/Solution.kt
@@ -7,16 +7,13 @@ fun readInput(name: String): List<String> = File("src/task03", "$name")
 
 fun part1(input: List<String>): Int {
     return input.map {
-        val a = it.subSequence(0, it.length / 2);
-        val b = it.subSequence(it.length / 2, it.length).toSet();
-        a.toSet().mapNotNull{it.takeIf{b.contains(it)}}.map{getCharScore(it) }.sum()
+        getCharScore(it.subSequence(it.length / 2, it.length).toSet().intersect(it.substring(0,it.length/2).toSet()).first());
     }.sum()
 }
 
 fun part2(input: List<String>): Int {
     return (0..input.size-1 step 3).map {
-        val a = input[it]; val b = input[it+1].toSet(); val c = input[it+2].toSet();
-        a.toSet().mapNotNull{it.takeIf{b.contains(it) and c.contains(it)}}.map{ getCharScore(it) }.sum()
+        getCharScore(input[it].toSet().intersect(input[it+1].toSet()).intersect(input[it+2].toSet()).first())
     }.sum()
 }
 
